4c6a71024e521d4dd733696e5d915363ac017663
Chris Offiong <chrisoffiong@gmail.com>
2018-12-12 01:55:34 -0500

    final

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function () {
     alert("Collect the crystals!");
-
+    
     var winningNumber = Math.floor(Math.random() * (120 - 19) + 19);
     var crystalOne = Math.floor(Math.random() * (20 - 1) + 1);
     var crystalTwo = Math.floor(Math.random() * (10 - 1) + 1);
@@ -10,7 +10,8 @@ $(document).ready(function () {
     var total = 0;
     var wins = 0;
     var losses = 0;
-
+    
+    
     function restart() {
         winningNumber = Math.floor(Math.random() * (120 - 19) + 19);
         crystalOne = Math.floor(Math.random() * (20 - 1) + 1);
@@ -20,20 +21,21 @@ $(document).ready(function () {
         total = 0;
         alert("Another Round!")
         alert("Click another crystal gem to play!")
-        $("#wins").html("Numbers of wins" + " " + wins)
-        $("#losses").html("Number of losses:" + " " + losses )
+       
     }
 
     function conditions() {
     if (total === winningNumber) {
         alert("Winner!");
         wins++;
+        $(".wins").html("Numbers of wins" + " " + wins)
         restart();
     }
     else if (total >= winningNumber) {
         alert("You collected too many!");
         losses++;
         restart();
+        $(".losses").html("Number of losses:" + " " + losses )
     }
     }
     $("#crystal1").on("click", function () {
